dd4859454be82ee085046c0b0fdd208a1dbfb5e6
ishikaagarwal01022 <ishikaagarwal01022@gmail.com>
2021-08-24 16:05:47 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll(".img-box img").forEach(image => {
+    image.onclick = () => {
+        document.querySelector(".popup-img").style.display = "block";
+        document.querySelector(".popup-img img").src = image.getAttribute('src')
+    }
+
+})
+
+document.querySelector(".popup-img span").onclick = () =>{
+    document.querySelector(".popup-img").style.display = "none"
+}
